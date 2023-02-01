f1fa3cb8743cdad992ad311547b02022d17c6f65
dhafa <dhafaalvaro15@gmail.com>
2023-02-01 11:03:06 +0700

    1ST COMMIT

---
+++ app/src/main/java/com/example/tabunganku/LoginActivity.kt
@@ -0,0 +1,75 @@
+package com.example.tabunganku
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.example.tabunganku.databinding.ActivityLoginBinding
+import com.rsz.latihanfirebase.ResetPasswordActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityLoginBinding
+    lateinit var auth : FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.forgotPassword.setOnClickListener {
+            val intent = Intent(this, ResetPasswordActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.tvToRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnLogin.setOnClickListener {
+            val email = binding.edtEmailLogin.text.toString()
+            val password = binding.edtPasswordLogin.text.toString()
+
+            //Validasi email
+            if (email.isEmpty()){
+                binding.edtEmailLogin.error = "Email Harus Diisi"
+                binding.edtEmailLogin.requestFocus()
+                return@setOnClickListener
+            }
+
+            //Validasi email tidak sesuai
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                binding.edtPasswordLogin.error = "Email Tidak Valid"
+                binding.edtPasswordLogin.requestFocus()
+                return@setOnClickListener
+            }
+
+            //Validasi password
+            if (password.isEmpty()){
+                binding.edtPasswordLogin.error = "Password Harus Diisi"
+                binding.edtPasswordLogin.requestFocus()
+                return@setOnClickListener
+            }
+
+            LoginFirebase(email,password)
+        }
+    }
+
+    private fun LoginFirebase(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "Selamat datang $email", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, DashboardActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
---
+++ app/src/main/java/com/example/tabunganku/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.tabunganku
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity () {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
