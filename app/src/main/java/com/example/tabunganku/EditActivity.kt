ef512f659fd6a527e3ea85861f03738d39b1f427
FahriMauludy <idnmuhammadfahri2008@gmail.com>
2023-02-13 09:09:42 +0700

    Nambahin Drawable

--- app/src/main/java/com/example/tabunganku/EditActivity.kt
+++ app/src/main/java/com/example/tabunganku/EditActivity.kt
@@ -1,11 +1,24 @@
 package com.example.tabunganku
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tabunganku.aplikasi.DashboardActivity
+import com.example.tabunganku.databinding.ActivityEditBinding
+import com.example.tabunganku.databinding.ActivityLoginBinding
 
 class EditActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityEditBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityEditBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit)
+        setContentView(binding.root)
+
+        binding.backButton.setOnClickListener{
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/tabunganku/aplikasi/DashboardActivity.kt
+++ app/src/main/java/com/example/tabunganku/aplikasi/DashboardActivity.kt
@@ -1,13 +1,28 @@
 package com.example.tabunganku.aplikasi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tabunganku.EditActivity
 import com.example.tabunganku.R
+import com.example.tabunganku.databinding.ActivityDashboardBinding
 
 class DashboardActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityDashboardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        setContentView(binding.root)
+
+       binding.iconMenuTambahkan.setOnClickListener{
+           val intent = Intent(this, EditActivity::class.java)
+           startActivity(intent)
+       }
+
+
+
 
 
     }
