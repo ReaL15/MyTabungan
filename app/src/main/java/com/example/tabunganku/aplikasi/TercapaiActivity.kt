155606e39cd405307b2fc2757eb4fef85e8bbcae
mulya <mulyaandara250@gmail.com>
2023-02-22 11:19:48 +0700

    tercapai xml

--- app/src/main/java/com/example/tabunganku/aplikasi/TercapaiActivity.kt
+++ app/src/main/java/com/example/tabunganku/aplikasi/TercapaiActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tabunganku.R
 
-class WithdrawActivity : AppCompatActivity() {
+class TercapaiActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_withdraw)
+        setContentView(R.layout.activity_tercapai)
     }
 }
