10638df9cbb1a67d31108bd01d98037189f72a73
dhafa <dhafaalvaro15@gmail.com>
2023-02-22 16:39:48 +0700

    Tugas Ana

--- app/src/main/java/com/example/tabunganku/aplikasi/SettingActivity.kt
+++ app/src/main/java/com/example/tabunganku/aplikasi/SettingActivity.kt
@@ -1,12 +1,42 @@
 package com.example.tabunganku.aplikasi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.tabunganku.LoginAndRegister.LoginActivity
 import com.example.tabunganku.R
+import com.example.tabunganku.databinding.ActivitySettingBinding
+import com.example.tabunganku.helper.PreferenceHelper
 
 class SettingActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySettingBinding
+    lateinit var sph : PreferenceHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySettingBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting)
+        val view = binding.root
+        setContentView(view)
+
+        sph = PreferenceHelper(this)
+
+        binding.btnBack.setOnClickListener{
+            startActivity(Intent(this, DashboardActivity::class.java))
+        }
+
+        binding.tombolLogout.setOnClickListener{
+            sph.clear()
+            Toast.makeText(applicationContext, "Berhasil Logout", Toast.LENGTH_SHORT).show()
+            moveIntent()
+        }
+
     }
+
+    private fun moveIntent() {
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
+    }
+
 }
